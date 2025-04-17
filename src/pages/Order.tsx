0daccba52065eb727087ff85b1e02638533e591a
IT22182678-Maleesha <maleesharashai2001@gmail.com>
2025-04-17 14:58:04 +0530

    Update Order.tsx

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -61,7 +61,8 @@ const Order: React.FC = () => {
   };
 
   return (
-    <div className="max-w-3xl mx-auto p-4">
+    <div className="max-w-3xl mx-auto pt-16 px-4 pb-4">
+
       <h1 className="text-3xl font-bold mb-4">Pizzas</h1>
 
       {pizzas.map((pizza) => (
