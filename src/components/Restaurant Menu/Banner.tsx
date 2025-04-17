63f3c7e35e5556c203426abb0856d86523919269
shaniranasinghe <it22371768@my.sliit.lk>
2025-04-17 12:02:02 +0530

    edit Banner.tsx

--- src/components/Restaurant Menu/Banner.tsx
+++ src/components/Restaurant Menu/Banner.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-const Banner: React.FC = () => (
-  <div className="w-full h-64 bg-black flex items-center justify-center relative">
-    
-    <div className="absolute left-0 top-0 w-full h-full flex flex-col items-center justify-center text-white">
-      <div className="text-4xl font-bold mb-2">Delicious Food</div>
-      <div className="flex gap-8">
-        <div className="bg-yellow-400 text-black px-4 py-2 rounded-lg font-semibold text-lg">2X1 ONLY TODAY!</div>
-        <div className="bg-yellow-400 text-black px-4 py-2 rounded-lg font-semibold text-lg">50% OFF</div>
-=======
 import bannerImg from "../../assets/images/sample_resturent_img.jpeg"; 
 const Banner = () => (
   <div className="w-full h-64 bg-black flex items-center justify-center relative">
@@ -27,14 +15,9 @@ const Banner = () => (
         <div className="bg-orange-500 text-black px-4 py-2 rounded-xl font-semibold text-lg">
           50% OFF
         </div>
->>>>>>> frontend
       </div>
     </div>
   </div>
 );
 
-<<<<<<< HEAD
 export default Banner;
-=======
-export default Banner;
->>>>>>> frontend
